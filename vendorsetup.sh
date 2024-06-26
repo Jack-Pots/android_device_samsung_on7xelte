18c3fd4bd3209835c78371b115573c3e94b8420e
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-06-26 16:33:26 +0530

    vendorsetup.sh: Clean up script
    Also fix clang clone

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,17 @@
 echo "Copying Toolchain"
-# rm -rf prebuilts/zyc_clang || true
-rm -rf prebuilts/toolchain || true
-# cp -R prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 prebuilts/toolchain
-git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 -b lineage-18.1 prebuilts/toolchain
-# git clone https://gitlab.com/clangsantoni/zyc_clang -b 14 prebuilts/zyc_clang
+
+# Clone GCC from Android 11
+if [ -d "prebuilts/toolchain" ]; then
+    echo "prebuilts/toolchain not found! Downloading..."
+    git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 -b lineage-18.1 prebuilts/toolchain  --depth=1
+fi
+
+# Clone Clang from Android 11
+if [ -d "prebuilts/toolchain-clang" ]; then
+    echo "prebuilts/toolchain-clang not found! Downloading..."
+    git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 -b android-11.0.0_r46 prebuilts/toolchain-clang  --depth=1
+fi
 
 # Create ld symlink to avoid linking error(s)
-git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 -b refs/tags/android-11.0.0_r46 prebuilts/toolchain-clang
 ln -s $(pwd)/prebuilts/toolchain-clang/clang-r383902b/bin/lld \
     $(pwd)/prebuilts/toolchain-clang/clang-r383902b/bin/ld &> /dev/null
